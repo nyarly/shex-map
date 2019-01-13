3ea5207982ec8f1e6808d2f10443b094e468348c
Judson <nyarly@gmail.com>
2019-01-13 13:43:58 -0800

    Working consumption from the manifest

--- spec/shex_map_spec.rb
+++ spec/shex_map_spec.rb
@@ -1,3 +1,4 @@
+require 'logger'
 require 'shex-map'
 require 'rdf/turtle'
 
@@ -49,13 +50,12 @@ RSpec::Matchers.define :have_same_statements_as do |expected|
 end
 
 describe ShExMap do
+  mf = RDF::Vocabulary.new("http://www.w3.org/2001/sw/DataAccess/tests/test-manifest#")
+  t = RDF::Vocabulary.new("https://raw.githubusercontent.com/shexSpec/shexmapTest/master/testcase.ttl#")
+
+
   shared_context "common configuration" do
     let(:map_iri) { RDF::URI.new("http://shex.io/extensions/Map/") }
-    let(:start_iri) { RDF::URI.new("http://example/foo") }
-    let(:target_iri) { RDF::URI.new("http://example/bar") }
-    let(:left_shape) { RDF::URI.new("http://a.example/S1") }
-    let(:right_shape) { RDF::URI.new("http://b.example/S1") }
-
     let(:prefixes) do
       {
         nil => RDF::URI.new("http://example/"),
@@ -94,24 +94,37 @@ describe ShExMap do
   end
 
 
-  shared_examples "shex-map" do |testcase|
+  shared_examples "shex-map" do |testcase, config|
     include_context "common configuration"
 
-    let(:testcase_directory) do
-      Pathname.new("testcases").join(testcase)
-    end
     let(:left_shex_path) do
-      testcase_directory.join("left.shex")
+      config.walk(t[:left]).value_at(t[:source]).to_s
     end
+    let(:left_shape) do
+      config.walk(t[:left]).value_at(t[:shape])
+    end
+
     let(:right_shex_path) do
-      testcase_directory.join("right.shex")
+      config.walk(t[:right]).value_at(t[:source]).to_s
     end
+    let(:right_shape) do
+      config.walk(t[:right]).value_at(t[:shape])
+    end
+
     let(:input_graph_path) do
-      testcase_directory.join("input.ttl")
+      config.walk(t[:start]).value_at(t[:source]).to_s
     end
+    let(:start_iri) do
+      config.walk(t[:start]).value_at(t[:focus])
+    end
+
     let(:expected_graph_path) do
-      testcase_directory.join("expected.ttl")
+      config.walk(t[:target]).value_at(t[:source]).to_s
     end
+    let(:target_iri) do
+      config.walk(t[:target]).value_at(t[:focus])
+    end
+
 
     let(:input_graph) do
       load_turtle(input_graph_path)
@@ -124,7 +137,7 @@ describe ShExMap do
     let(:left_shex) { ShEx.parse(File.read(left_shex_path), **parse_options) }
     let(:right_shex) { ShEx.parse(File.read(right_shex_path), **parse_options) }
 
-    context "loading testcases from 'testcases/#{testcase}'" do
+    context testcase do
       before do
         left_shex.execute(input_graph, {start_iri => left_shape})
       end
@@ -163,11 +176,44 @@ describe ShExMap do
     end
   end
 
-  Dir.open("testcases") do |tc_dir|
-    tc_dir.each do |name|
-      next if /^\./ =~ name
-      FileTest.directory?("testcases/#{name}")
-      it_should_behave_like "shex-map", name
+  testcases = RDF::Graph.new
+
+  RDF::Reader.open('testcases/manifest.ttl', base_uri: "./testcases/") do |r|
+    r.each_statement do |stmt|
+      testcases.insert(stmt)
     end
   end
+
+  class Thumb < Struct.new(:graph, :subject)
+    def value_at(predicate)
+      graph.query([subject, predicate, nil]).first.object
+    end
+
+    def walk(predicate)
+      Thumb.new(graph, graph.query([subject, predicate, nil]).first.object)
+    rescue
+      fail "No statement matches [#{subject}, #{predicate}, nil]"
+    end
+
+    def statements_about
+      graph.query([subject, nil, nil]).to_a
+    end
+  end
+
+
+
+  entries = testcases.query([RDF::URI.new("./testcases/"), mf[:entries], nil]).first.object
+  entries = RDF::List.new(graph: testcases, subject: entries)
+  entries.each do |entry|
+    thumb = Thumb.new(testcases, entry)
+    it_should_behave_like "shex-map", thumb.value_at(mf[:name]).to_s, thumb.walk(mf[:action])
+  end
+
+#  Dir.open("testcases") do |tc_dir|
+#    tc_dir.each do |name|
+#      next if /^\./ =~ name
+#      FileTest.directory?("testcases/#{name}")
+#      it_should_behave_like "shex-map", name
+#    end
+#  end
 end
